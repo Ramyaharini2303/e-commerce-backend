098d86ef9d937c6656ba6fa682ced1b434ca2081
Ramyaharini2303 <ramyaharini2303@gmail.com>
2025-05-14 18:08:54 -0400

    initial commit

---
+++ src/test/java/com/e_commerce_app/e_commerce_app_backend/ECommerceAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerce_app.e_commerce_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
